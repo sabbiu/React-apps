6baa361dc05d6389059b2b9e0fd2fdfe3516b58e
Sabbiu Shah <Sabbiu Shah>
2017-01-13 00:09:23 +0545

    Nesting on React

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,38 @@
+var GreeterMessage = React.createClass({
+	render:function(){
+		var name = this.props.name,
+				msg = this.props.msg;
+
+		return (
+			<div>
+				<h1>Hello {name}!</h1>
+				<p>{msg + '!!'}</p>
+			</div>
+		);
+	}
+});
+
+var GreeterForm = React.createClass({
+	onFormSubmit: function(e) {
+		e.preventDefault();
+
+		var name = this.refs.name.value;
+
+		if (name.length>0) {
+			this.refs.name.value = '';
+			this.props.onNewName(name);
+		}
+	},
+	render: function() {
+		return (
+			<form onSubmit={this.onFormSubmit}>
+				<input type="text" ref="name"/>
+				<button>Set Name</button>
+			</form>
+		);
+	}
+});
+
 var Greeter = React.createClass({
 	getDefaultProps: function() {
 		return {
@@ -10,35 +45,19 @@ var Greeter = React.createClass({
 			name: this.props.name,
 		};
 	},
-	onButtonClick: function(e) {
-		e.preventDefault();
-
-		var nameRef = this.refs.name;
-
-		var name = nameRef.value;
-		nameRef.value = '';
-
-		if (typeof name === 'string' && name.length > 0) {
-			this.setState({
-				name: name
-			});
-		}
-		
-		// alert(name);
+	handleNewName: function(name) {
+		this.setState({
+			name: name
+		});
 	},
 	render: function() {
-		console.log(this.props);
+		// console.log(this.props);
 		var name = this.state.name,
 				msg = this.props.msg;
 		return (
 			<div>
-				<h1>Hello {name}!</h1>
-				<p>{msg + '!!'}</p>
-
-				<form onSubmit={this.onButtonClick}>
-					<input type="text" ref="name"/>
-					<button>Set Name</button>
-				</form>
+				<GreeterMessage name={name} msg={msg}/>
+				<GreeterForm onNewName={this.handleNewName}/>
 			</div>
 		)
 	}
