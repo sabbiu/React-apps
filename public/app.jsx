acffe426b1fcaf93f472f3984090bd829a60e612
Sabbiu Shah <Sabbiu Shah>
2017-01-12 23:46:40 +0545

    Modification state

--- public/app.jsx
+++ public/app.jsx
@@ -12,11 +12,18 @@ var Greeter = React.createClass({
 	},
 	onButtonClick: function(e) {
 		e.preventDefault();
-		var name = this.refs.name.value;
 
-		this.setState({
-			name: name
-		})
+		var nameRef = this.refs.name;
+
+		var name = nameRef.value;
+		nameRef.value = '';
+
+		if (typeof name === 'string' && name.length > 0) {
+			this.setState({
+				name: name
+			});
+		}
+		
 		// alert(name);
 	},
 	render: function() {
