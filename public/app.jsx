89b0f3fad4575478849581a8c45e543787542e8b
Sabbiu Shah <Sabbiu Shah>
2017-01-12 23:31:07 +0545

    Working with properties"Props"

--- public/app.jsx
+++ public/app.jsx
@@ -1,20 +1,28 @@
 var Greeter = React.createClass({
+	getDefaultProps: function() {
+		return {
+			name: 'React',
+			msg: 'This is from a component'
+		};
+	},
+
 	render: function() {
-		return React.createElement(
-			'h1',
-			null,
-			'Hello React.createElement'
+		// console.log(this.props);
+		var name = this.props.name,
+				msg = this.props.msg;
+		// console.log(name);
+		return (
+			<div>
+				<h1>Hello {name}!</h1>
+				<p>{msg + 'Ho ha ha ha hu haha ha aha ha'}</p>
+			</div>
 		)
-		// return (
-		// 	<div>
-		// 		<h1>Hello React!</h1>
-		// 		<p>This is from a component</p>
-		// 	</div>
-		// )
 	}
 });
 
+var name = 'Sabbiu',
+		msg = 'Hey, whats up!';
 ReactDOM.render(
-	<Greeter/>,
+	<Greeter name={name} msg={msg}/>,
 	document.getElementById('app')
 );
